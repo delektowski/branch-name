1b29abac98eb6ffecc9acebc1d25d692bcda7f29
Marcin <delektowski@interia.pl>
2022-02-11 10:58:12 +0100

    added new format

--- branch-name.sh
+++ branch-name.sh
@@ -33,8 +33,8 @@ branch_converted=$OUTPUT
 
 # Output
 branch_convert_to_lowercase=`echo $branch_converted | awk '{print tolower($0)}'`
-task_convert_to_uppercase=`echo $task | awk '{print toupper($0)}'`
+task_convert_to_uppercase=`echo $task | awk '{print tolower($0)}'`
 IFS=' '
 read -ra arr <<< "$branch_convert_to_lowercase" 
 concatenate_elements=$(IFS=- ; echo "${arr[*]}")
-git checkout -b "$task_convert_to_uppercase-$concatenate_elements"
+git checkout -b "$task_convert_to_uppercase/$concatenate_elements"
